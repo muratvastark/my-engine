8f53b48fc2a95ca2ce8040156dbd1ab9809b1dfa
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-10 00:49:40 +0300

    Create snipe.js

---
+++ src/Commands/General/snipe.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (Moderation, message, args) => {
+    if (message.member.roles.highest.positon < message.guild.roles.cache.get(Moderation.Defaults.MinStaffRole).position) return;
+
+    const Channel = message.guild.channels.cache.get(args[0]) || message.channel;
+    const Snipes = Moderation.Snipes[Channel.id];
+
+    if (!Snipes.length) return message.channel.send(`${Channel} kanalında bot açıkken hiç mesaj silinmemiş.`);
+
+    const Embed = new MessageEmbed().setColor("RANDOM").setDescription(`${Channel} kanalında en son silinen 5 mesaj;`);
+
+    Snipes.map((data) => Embed.addField(data.Writer, `${data.Content ? data.Content + " - " : ""}${Moderation.timeTR(Date.now() - data.DeletedAt)}${data.Image ? ` - [Dosya](${data.Image})` : ""}`));
+
+    message.channel.send(Embed);
+};
+
+exports.conf = {
+    commands: ["snipe", "snipes"],
+    enabled: true,
+    usage: "snipe [Kanal]"
+};
