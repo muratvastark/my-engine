ab18e81b90b445726f64b678f6a00ca13049e525
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-10 20:02:55 +0300

    Update snipe.js

--- src/Commands/General/snipe.js
+++ src/Commands/General/snipe.js
@@ -6,7 +6,7 @@ exports.run = async (Moderation, message, args) => {
     const Channel = message.guild.channels.cache.get(args[0]) || message.channel;
     const Snipes = Moderation.Snipes[Channel.id];
 
-    if (!Snipes.length) return message.channel.send(`${Channel} kanalında bot açıkken hiç mesaj silinmemiş.`);
+    if (Snipes.length < 1) return message.channel.send(`${Channel} kanalında bot açıkken hiç mesaj silinmemiş.`);
 
     const Embed = new MessageEmbed().setColor("RANDOM").setDescription(`${Channel} kanalında en son silinen 5 mesaj;`);
 
