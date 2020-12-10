3d508bd256627bba5a2a4db6ad15864faf176b0c
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-10 21:30:41 +0300

    Create slow-mode.js

---
+++ src/Commands/Advanced/slow-mode.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (Moderation, message, [ option ]) => {
+    if (message.member.check() === false) return;
+    if (!option || !Number(option) || option < 0) return message.channel.send("Geçerli bir saniye belirt. (Yavaş modu kaldırmak için 0 yazabilirsin.)");
+    message.channel.setRateLimitPerUser(option, `${message.author.tag} tarafından istendi.`);
+    message.channel.send(new MessageEmbed().setColor("RANDOM").setDescription(`Başarıyla yavaş mod ${option === 0 ? "kaldırıldı." : `**${option} saniye** olarak kaldırıldı.`}`));
+};
+
+exports.conf = {
+    commands: ["slow-mode", "yavaş-mod", "yavaşmod", "slowmode"],
+    enabled: true,
+    usage: "slow-mode [Sayı]"
+};
