9f16dafda2acbef299bccf3b0190aa415c3d23fc
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-11 18:12:03 +0300

    Create reset-stats.js

---
+++ src/Commands/Advanced/reset-stats.js
@@ -0,0 +1,17 @@
+const { StatsModel } = require("../../Helpers/models.js");
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (Moderation, message, args) => {
+    if (message.guild.ownerID !== message.author.id || !Moderation.Defaults.Developers.includes(message.author.id)) return;
+    const Embed = new MessageEmbed().setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true })).setColor("RANDOM");
+    
+    const newData = new Map();
+    await Stats.updateMany({}, { Voice: newData, Message: newData });
+    
+};
+
+exports.conf = {
+    commands: ["reset-stats"],
+    enabled: true,
+    usage: "reset-stats"
+};
