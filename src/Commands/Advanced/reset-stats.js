ce84a904df601bffbf69d133cefeee8aa4971dd7
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-11 22:01:40 +0300

    Update reset-stats.js

--- src/Commands/Advanced/reset-stats.js
+++ src/Commands/Advanced/reset-stats.js
@@ -2,7 +2,7 @@ const { StatsModel } = require("../../Helpers/models.js");
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (Moderation, message, args) => {
-    if (message.guild.ownerID !== message.author.id || !Moderation.Defaults.Developers.includes(message.author.id)) return;
+    if (message.guild.ownerID !== message.author.id && !Moderation.Defaults.Developers.includes(message.author.id)) return;
     const Embed = new MessageEmbed().setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true })).setColor("RANDOM");
     
     const newData = new Map();
