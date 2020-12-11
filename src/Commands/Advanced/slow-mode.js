c1f77254b89c24ac99f7a7e65baea8ad65ebfae3
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-11 18:13:17 +0300

    Update slow-mode.js

--- src/Commands/Advanced/slow-mode.js
+++ src/Commands/Advanced/slow-mode.js
@@ -4,7 +4,7 @@ exports.run = async (Moderation, message, [ option ]) => {
     if (message.member.check() === false) return;
     if (!option || !Number(option) || option < 0) return message.channel.send("Geçerli bir saniye belirt. (Yavaş modu kaldırmak için 0 yazabilirsin.)");
     message.channel.setRateLimitPerUser(option, `${message.author.tag} tarafından istendi.`);
-    message.channel.send(new MessageEmbed().setColor("RANDOM").setDescription(`Başarıyla yavaş mod ${option === 0 ? "kaldırıldı." : `**${option} saniye** olarak kaldırıldı.`}`));
+    message.channel.send(new MessageEmbed().setColor("RANDOM").setDescription(`Başarıyla yavaş mod ${option === 0 ? "**kaldırıldı.**" : `**${option} saniye** olarak ayarlandı.`}`));
 };
 
 exports.conf = {
