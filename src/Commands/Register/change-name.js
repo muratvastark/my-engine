92716b59d82454b8757db2e8867c58de7c73b055
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-11 22:12:17 +0300

    Update change-name.js

--- src/Commands/Register/change-name.js
+++ src/Commands/Register/change-name.js
@@ -1,10 +1,10 @@
-const { BoosterRole, SecondTag, Tag, MinStaffRole } = global.Moderation.Defaults;
+const { BoosterRole, SecondTag, Tag } = global.Moderation.Defaults;
 const { Register } = global.Moderation.Permissions;
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async (Moderation, message, args) => {
     if (message.member.roles.cache.has(BoosterRole) && message.mentions.users.size <= 0) {
-        if (message.member.check(Register.AuthRoles) === false && (!args || !args.length)) return message.reply("sunucudaki ismini mi değiştireceksin? Değiştireceksen bir isim girmelisin.");
+        if (!args || !args.length   ) return message.reply("sunucudaki ismini mi değiştireceksin? Değiştireceksen bir isim girmelisin.");
         const Name = `${Member.user.username.includes(Tag) ? Tag : SecondTag} ${args.join(" ")}`;
         if (Name.length > 30) return message.reply("ismin 30 karakterden büyük olamaz.");
         message.member.setNickname(isim);
@@ -15,7 +15,7 @@ exports.run = async (Moderation, message, args) => {
     if (message.member.check(Register.AuthRoles) === false) return;
     const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if (!Member || Member.user.bot || Member.id === message.author.id) return message.channel.send("Geçerli bir üye belirt!");
-    if (!Member.manageable || Member.roles.highest.position >= message.guild.roles.cache.get(MinStaffRole).position) return message.channel.send("Yetkili olmayan bir üye belirt!")
+    if (!Member.manageable || Member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("Yetkili olmayan bir üye belirt!")
 
     args = args.filter(a => a !== "" && a !== " ").splice(1);
     const Name = args.filter(arg => isNaN(arg)).map(arg => arg.charAt(0).replace('i', "İ").toUpperCase()+arg.slice(1)).join(" ");
