075d9d3add486cb86124b7d28fd86a26fb00bb68
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-11 23:04:49 +0300

    Create man.js

---
+++ src/Commands/Register/man.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const { Tag, TagIntakeMode, TeamRole, MinStaffRole, BoosterRole, ChatChannel } = global.Moderation.Defaults;
+const { Register } = global.Moderation.Permissions;
+
+exports.run = async (Moderation, message, args) => {
+    if (message.member.check(Register.AuthRoles) === false) return;
+
+    const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if (!Member || Member.user.bot || Member.id === message.author.id) return message.channel.send("Geçerli bir üye belirt!");
+
+    if (!Member.manageable || Member.roles.highest.position >= message.guild.roles.cache.get(MinStaffRole).position) return message.channel.send("Yetkili olmayan bir üye belirt!");
+    if (message.member.check() === false && TagIntakeMode === true && !Member.user.username.includes(Tag) && !Member._roles.includes(BoosterRole)) return message.channel.send("İsminde tag bulunan birilerini kaydet.");
+
+    Member.setRoles(Register.ManRoles);
+    message.channel.send(new MessageEmbed().setColor("RANDOM").setDescription(`${Member.toString()} kişisi başarıyla \`erkek\` olarak kaydedildi.`));
+    if (Member.user.username.includes(Tag)) Member.roles.add(TeamRole).catch(console.error);
+    const Channel = Moderation.channels.cache.get(ChatChannel);
+    if (Channel) Channel.send(`${Member} aramıza katıldı.`);
+    global.updateUser(message.author.id, "Man", 1);
+    global.addName(Member.id, Member.displayName, `<@&${Register.ManRoles[0]}>`);
+};
+
+exports.conf = {
+    commands: ["e", "erkek", "bay"],
+    enabled: true,
+    usage: "e [Üye]"
+};
