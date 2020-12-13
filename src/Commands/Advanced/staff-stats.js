7259e4bd4e4559c0043aeab79d1130f78cda5749
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-13 20:31:01 +0300

    Update staff-stats.js

--- src/Commands/Advanced/staff-stats.js
+++ src/Commands/Advanced/staff-stats.js
@@ -3,10 +3,9 @@ const { StatsModel, InviteModel } = require("../../Helpers/models.js");
 const { MessageEmbed, Util } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
-const Mongoose = require("mongoose");
 
 exports.run = async (Moderation, message, args) => {
-    if (message.guild.ownerID !== message.author.id) return;
+    if (message.guild.ownerID !== message.author.id && !Moderation.Defaults.Developers.includes(message.author.id)) return;
     const Embed = new MessageEmbed().setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true })).setColor("RANDOM");
     const Options = message.guild.roles.cache.filter(role => role.members.size > 2 && role.position >= message.guild.roles.cache.get(MinStaffRole).position).map(role => role.name.toLowerCase()).concat("all");
     const Option = args.join(" ").split("--")[1];
@@ -30,9 +29,9 @@ exports.run = async (Moderation, message, args) => {
     }
 
     const arr = Util.splitMessage(Message, { maxLength: 2000, char: "\n" });
+    Embed.addField("** **", `Yukarıdaki bilgiler **${message.guild.name}** sunucusunun **${Option === "all" ? "genel" : `${message.guild.roles.cache.find(_role => _role.name.toLowerCase() === Option).toString()}  rolünün`}** haftalık mesaj, haftalık ses ve haftalık invite istatistikleridir.`);
     for (const newText of arr) {
         Embed.setDescription(newText);
-        Embed.addField("** **", `Yukarıdaki bilgiler **${message.guild.name}** sunucusunun **${Option === "all" ? "genel" : `${message.guild.roles.cache.find(_role => _role.name.toLowerCase() === Option).toString()}  rolünün`}** haftalık mesaj, haftalık ses ve haftalık invite istatistikleridir.`);
         message.channel.send(Embed);
     }
     
