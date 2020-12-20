c6a44da3b92a3a2ea21f4288bd2d8ab92404a117
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-21 00:28:40 +0300

    Update jail.js

--- src/Commands/Penal/jail.js
+++ src/Commands/Penal/jail.js
@@ -10,7 +10,7 @@ exports.run = async (Moderation, message, args) => {
     if (!Member) return message.channel.send("Cezalandırmak için bir üye belirt!")
     if (Member.roles && message.author.id !== message.guild.ownerID && Member.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("Kişi senden yüksek bir pozisyona sahip, onu susturamazsın!")
     if (Member.roles && !Member.manageable) return message.channel.send("Etiketlediğin kişi benden yüksek bir role sahip, üzgünüm.")
-    if (Member.id && Member.id === message.author.id) return message.channel.send("Kendini susturmayı neden deniyorsun ki?")
+    if (Member.id && Member.id === message.author.id) return message.channel.send("Kendini cezalandırmayı neden deniyorsun ki?")
     if (message.guild.ownerID !== message.author.id && Penal.limit(message.author.id, jailLimit, Jail.Limit) === false) return message.channel.send(" `Jail` limitine ulaştınınız.")
     const Reason = args.slice(2).join(" ") || "Sebep Belirtilmedi.";
     if (!Reason) return message.channel.send("Geçerli bir sebep belirt.");
@@ -21,10 +21,10 @@ exports.run = async (Moderation, message, args) => {
     const NewPenal = await Penal.addPenal(Member.id, message.author.id, "JAIL", Reason, false, DateNow, undefined, Member.roles ? Member.roles.cache.map((x) => x.id) : null)
     const Embed = new MessageEmbed().setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }));
 
-    message.channel.send(Embed.setColor("RANDOM").setDescription((Member.roles ? `${Member} (\`${Member.id}\`) üyesi` : `**${Member.tag}** (\`${Member.id}\`) kullanıcısı sunucuda olmamasına rağmen`) + ` cezalıya atıldı. (\`#${NewPenal.Id}\`)`));
+    message.channel.send(Embed.setColor("RANDOM").setDescription(`${Member} (\`${Member.id}\`) üyesi cezalıya atıldı. (\`#${NewPenal.Id}\`)`));
 
     const Channel = message.guild.channels.cache.get(Jail.Channel);
-    if (Channel) Channel.send(Embed.setDescription((Member.roles ? `${Member} (\`${Member.id}\`) üyesi` : `**${Member.tag}** (\`${Member.id}\`) kullanıcısı sunucuda olmamasına rağmen`) + ` ${message.author} tarafından cezalıya atıldı. Sebep: \`${Reason}\``).setColor("RED"));
+    if (Channel) Channel.send(Embed.setDescription(`${Member} (\`${Member.id}\`) üyesi ${message.author} tarafından cezalıya atıldı. Sebep: \`${Reason}\``).setColor("RED"));
 };
 
 exports.conf = {
