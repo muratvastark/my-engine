95f796d105f3cce083de545c3165cb97e6a7ccfe
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2020-12-21 00:26:37 +0300

    Update transport.js

--- src/Commands/Advanced/transport.js
+++ src/Commands/Advanced/transport.js
@@ -2,20 +2,22 @@ const { GuildMember, VoiceChannel, MessageEmbed } = require("discord.js");
 
 exports.run = async (Moderation, message, args) => {
   if (message.member.check(Moderation.Defaults.TransportAuthRoles) === false) return;
- 
-  const victim = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-  if (!args[0] || !victim || victim.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("Geçerli bir üye belirt!");
+
+  let victim = message.mentions.members.first() || message.guild.member(args[0]);
+  if (!args[0] || !victim) return message.channel.send("Taşınacak üyeyi etiketle!");
   if (!victim.voice.channelID) return message.channel.send("Taşınacak üye bir ses kanalında değil!");
+  else if (victim.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("Taşınacak üye senden daha yetkili!");
   
-  const target = message.mentions.members.array()[1] || message.guild.members.cache.get(args[1]) || message.guild.channels.cache.find(x => x.type == "voice" && x.id == args[1]);
-  if (!args[1] || !target) return message.channel.send("Geçerli bir üyeyi veya ses kanalı belirt!");
+  let target = message.mentions.members.array()[1] || message.guild.member(args[1]) || message.guild.channels.cache.find(x => x.type == "voice" && x.id == args[1]) || message.guild.channels.cache.find(x => x.type == "voice" && x.name.toLowerCase().includes(args.splice(1).join(" ").toLowerCase()));
+  if (!args[1] || !target) return message.channel.send("Hedef üyeyi veya ses kanalını belirt! (ID veya isim ile)");
   
-  const isMember = target instanceof GuildMember;
+  let isMember = target instanceof GuildMember;
+  
   if (isMember && !target.voice.channelID) return message.channel.send("Hedef üye bir ses kanalında değil!");
   else if (isMember && target.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("Hedef üye senden daha üstte bir role sahip!");
   
-  if (victim.voice) victim.voice.setChannel(isMember ? target.voice.channelID : target.id);
-  message.channel.send(new MessageEmbed().setAuthor(message.author.tag, message.author.avatarURL({ dynamic:true })).setColor("RANDOM").setDescription(`${victim.toString()} üyesi başarı ile **${target.toString()}** ${isMember ? `üyesinin ses kanalına (**${target.voice.channel.name}**) taşındı.` : `ses kanalında taşındı!`}`));
+  victim.voice.setChannel(isMember ? target.voice.channelID : target.id).catch();
+  message.channel.send(new MessageEmbed().setColor("RANDOM").setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setDescription(`${message.author} tarafından, ${victim} üyesi ${target instanceof GuildMember ? `üyesinin ses kanalına (**${target.voice.channel.name}**) taşındı.` : `ses kanalında taşındı!`}`));
 };
 
 exports.conf = {
