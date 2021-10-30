1f9fa80df497ba719b80b3781cb75cfbdfca1023
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2021-10-30 17:18:36 +0300

    30.10.2021 update
    canım sıkıldı kanka kötü gözüktü kod bir elden geçireyim dedim :d

--- src/OwnerGuard.js
+++ src/OwnerGuard.js
@@ -1,39 +1,41 @@
 const { Client, Guild } = require("discord.js");
-const Guard = new Client();
-const Settings = require("../global.json").Guard;
-const SpamCounter = {};
-const Permissions = ["ADMINSTRATOR", "KICK_MEMBERS", "MANAGE_GUILD", "BAN_MEMBERS", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_NICKNAMES", "MANAGE_CHANNELS"];
+const settings = require("../global.json").Guard;
 
-Guard.on("roleCreate", async(role) => await role.guild.checkLog());
-Guard.on("roleDelete", async(role) => await role.guild.checkLog());
-Guard.on("roleUpdate", async(oldRole) => await oldRole.guild.checkLog());
-Guard.on("channelCreate", async(channel) => await channel.guild.checkLog());
-Guard.on("channelDelete", async(channel) => await channel.guild.checkLog());
-Guard.on("channelUpdate", async(oldChannel) => await oldChannel.guild.checkLog());
-Guard.on("guildUpdate", async(oldGuild) => await oldGuild.checkLog());
-Guard.on("webhookUpdate", async(oldWebhook) => await oldWebhook.guild.checkLog());
-Guard.on("guildBanAdd", async(guild) => await guild.checkLog());
-Guard.on("guildMemberRemove", async(member) => await member.guild.checkLog());
-Guard.on("guildMemberAdd", async(member) => await member.guild.checkLog());
-Guard.on("guildMemberUpdate", async(oldMember, newMember) => {
+const client = new Client();
+const permissions = ["ADMINSTRATOR", "KICK_MEMBERS", "MANAGE_GUILD", "BAN_MEMBERS", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_NICKNAMES", "MANAGE_CHANNELS"];
+let limits = [];
+
+client.on("roleCreate", async(role) => await role.guild.checkLog());
+client.on("roleDelete", async(role) => await role.guild.checkLog());
+client.on("roleUpdate", async(oldRole) => await oldRole.guild.checkLog());
+client.on("channelCreate", async(channel) => await channel.guild.checkLog());
+client.on("channelDelete", async(channel) => await channel.guild.checkLog());
+client.on("channelUpdate", async(oldChannel) => await oldChannel.guild.checkLog());
+client.on("guildUpdate", async(oldGuild) => await oldGuild.checkLog());
+client.on("guildBanAdd", async(guild) => await guild.checkLog());
+client.on("guildMemberRemove", async(member) => await member.guild.checkLog());
+client.on("guildMemberAdd", async(member) => await member.guild.checkLog());
+
+client.on("guildMemberUpdate", async(oldMember, newMember) => {
     if (oldMember._roles === newMember._roles) return;
-    const changedRoles = newMember.roles.cache.filter((_role) => !oldMenber.roles.cache.has(_role.id)).filter((_role) => Permissions.some((_perm) => _role.permissions.toArray().includes(_perm)));
-    if(changedRoles.size === 0) return;
-    oldMember.guild.checkLog();
+    const changedRoles = newMember.roles.cache.filter((role) => !oldMember.roles.cache.has(role.id) && permissions.some((perm) => role.permissions.toArray().includes(perm)));
+    if(changedRoles.size !== 0) await oldMember.guild.checkLog();
+});
+
+client.on("voiceStateUpdate", (oldState, newState) => {
+  if (oldState.channelID && !newState.channelID) await oldMember.guild.checkLog();
 });
 
 Guild.prototype.checkLog = async function() {
-    const Log = await this.fetchAuditLogs({ limit: 1 }).then((_audit) => _audit.entries.first());
-    if (!Log || Date.now() - Log.createdTimestamp > 5000) return true;
-    if (Member.roles.highest.position < Guild.roles.cache.get(Settings.MinStaffRole).position) return;
-    const Member = await this.members.fetch(Log.executor.id);
-    if (Settings.MaxWarn > SpamCounter[log.executor.id] && Member) Member.roles.set(Member.roles.cache.filter((_role) => Permissions.some((_perm) => _role.permissions.toArray().includes(_perm))).map((role) => role.id));
-    else {
-        SpamCounter[Log.executor.id] = SpamCounter[Log.executor.id] ? SpamCounter[Log.executor.id] + 1 : 1;
-        setTimeout(() => { 
-            SpamCounter[Log.executor.id] > 0 ? SpamCounter[Log.executor.id] = 0 : delete SpamCounter[Log.executor.id];
-        }, 1000 * 60 * 3);    
-    }
+    const audit = await this.fetchAuditLogs({ limit: 1 }).then((audit) => audit.entries.first());
+    const now = Date.now();
+    if (!audit || now - audit.createdTimestamp > 5000) return true;
+
+    limits = limits.filter(limit => now - limit.timestamp > 1000 * 60 * 15);
+    limits.push({ user: audit.executor.id, timestamp: audit.createdTimestamp, type: audit.actionType });
+
+    const userLimits = limits.filter(limit => now - limit.timestamp < 1000 * 60 * 15 && limit.user === audit.executor.id && limit.type === audit.actionType);
+    if (userLimits > 5) this.guild.members.kick(audit.executor.id).catch(() => undefined));
 };
 
-Guard.login(Settings.Token).then(() => console.log(`${Guard.user.tag} is online!`)).catch(() => Guard.destroy());
+client.login(Settings.Token).then(() => console.log(`${client.user.tag} is online!`));
